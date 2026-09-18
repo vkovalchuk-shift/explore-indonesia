3d28f74687f6d75ba8edec405b8b68cfdf4e8066
vkovalchuk-shift <v.kovalchuk@cleack.com>
2026-09-18 11:46:12 +0300

    create new sections; remove Web Components

--- src/js/index.js
+++ src/js/index.js
@@ -1,53 +1,25 @@
 import "../styles/main.scss";
 
 document.addEventListener('DOMContentLoaded', function () {
-  class SecretsSlider extends HTMLElement {
-    constructor () {
-      super();
-      this.swiper = null;
-    }
-
-    connectedCallback () {
-      this.swiperEl = this;
-      this.pagination = this.querySelector('.swiper-pagination');
-      this.initSwiper();
-    }
-
-    initSwiper () {
-      if (!this.swiperEl) return;
-
-      this.swiper = new Swiper(this.swiperEl, {
-        loop: true,
-        slidesPerView: 1,
-        spaceBetween: 31,
-        pagination: {
-          el: this.pagination,
-          clickable: true
-        },
-        // navigation: {
-        //   nextEl: '.arrow-right',
-        //   prevEl: '.arrow-left'
-        // },
-        breakpoints: {
-          576: {
-            slidesPerView: 2
-          },
-          1200: {
-            slidesPerView: 3
-          }
-        }
-      });
-    }
-
-    disconnectedCallback() {
-      if (this.swiper && !this.swiper.destroyed) {
-        this.swiper.destroy(true, true);
-        this.swiper = null;
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 31,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.arrow-next',
+      prevEl: '.arrow-prev'
+    },
+    breakpoints: {
+      576: {
+        slidesPerView: 2
+      },
+      1200: {
+        slidesPerView: 3
       }
     }
-  }
-
-  if (!customElements.get('secrets-slider')) {
-    customElements.define('secrets-slider', SecretsSlider)
-  }
+  });
 });
