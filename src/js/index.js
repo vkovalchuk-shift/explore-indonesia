371a1c95fd828082bfa7e97b3bb2c5c8842827a6
vkovalchuk-shift <v.kovalchuk@cleack.com>
2026-09-18 16:29:24 +0300

    create header and footer

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,16 @@
 import "../styles/main.scss";
 
 document.addEventListener('DOMContentLoaded', function () {
+  const header = document.querySelector('header');
+  
+  document.getElementById('menu-button').onclick = function() { 
+    this.classList.toggle('active');
+  }
+
+  window.addEventListener('scroll', () => {
+    header.classList.toggle('blurred', window.scrollY >= header.offsetHeight);
+  });
+
   const swiper = new Swiper('.swiper', {
     loop: true,
     slidesPerView: 1,
